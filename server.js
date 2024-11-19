fa270922921c8935222424a04825ff9d8c65a68c
Guilherme Lopes <guifla330@gmail.com>
2024-11-19 13:06:42 -0300

    Atualização server.js
    - Implementando lista de arrays "posts";
    - Criando função para buscar post através do ID;
    - Criando requisições da lista "posts"

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
 import express from "express";
 
+const posts = [
+  {
+    id: 1,
+    descricao: "Amizade felina.",
+    imagem: "https://placecats.com/millie_neo/300/200",
+  },
+  {
+    id: 2,
+    descricao: "Gato comendo banana.",
+    imagem: "https://placecats.com/neo_banana/300/200",
+  },
+  {
+    id: 3,
+    descricao: "Gato vesgo.",
+    imagem: "https://placecats.com/neo_2/300/200",
+  },
+];
+
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Servidor funcionando.");
 });
 
-app.get("/api", (req, res) => {
-  res.status(200).send("Mensagem do servidor!");
+app.get("/posts", (req, res) => {
+  res.status(200).json(posts);
+});
+
+function buscarPostPorID(id) {
+  return posts.findIndex((post) => {
+    return post.id === Number(id);
+  });
+}
+
+app.get("/posts/:id", (req, res) => {
+  const index = buscarPostPorID(req.params.id);
+  res.status(200).json(posts[index]);
 });
