3bb8acdab45f91ca314ed30bd75bf2be793bf675
Guilherme Lopes <guifla330@gmail.com>
2024-11-20 13:35:09 -0300

    Add general implementations
    - Added src folder with database config, controllers, models and routes.
    - Implemented many functions that will colaborate with Mongo Database.

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import routes from "./src/routes/postsRoutes.js";
+
 
 const posts = [
   {
@@ -19,24 +21,7 @@ const posts = [
 ];
 
 const app = express();
-
-app.use(express.json());
-
+routes(app)
 app.listen(3000, () => {
   console.log("Servidor funcionando.");
-});
-
-app.get("/posts", (req, res) => {
-  res.status(200).json(posts);
-});
-
-function buscarPostPorID(id) {
-  return posts.findIndex((post) => {
-    return post.id === Number(id);
-  });
-}
-
-app.get("/posts/:id", (req, res) => {
-  const index = buscarPostPorID(req.params.id);
-  res.status(200).json(posts[index]);
 });
